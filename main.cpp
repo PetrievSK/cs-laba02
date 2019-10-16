c30a427f130a010d9a2188861825ca7289dae29b
Alice (PetrievSK) <petrievsergey@yandex.ru>
2019-10-17 00:19:54 +0300

    code: добавление деления от Алисы

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
-     << "A * B = " << a * b << '\n';
+     << "A * B = " << a * b << '\n'
+     << "A / B = " << a / b << '\n';
     return 0;
 }
